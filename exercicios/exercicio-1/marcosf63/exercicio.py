e54d7da067f4d80b5bf2230b22cfe2c1a826af87
marcosf63 <marcosf63@gmail.com>
2016-01-07 13:40:29 -0300

    update

--- exercicios/exercicio-1/marcosf63/exercicio.py
+++ exercicios/exercicio-1/marcosf63/exercicio.py
@@ -5,10 +5,13 @@ def lerImprimeVetor():
   """
    Ler um vetor de 5 números inteiros e imprime na tela.
   """
-  vetor = [raw_input('Digite o número %d: ' % i) for i in range(1,6)]
+  try:
+   vetor = [int(raw_input('Digite o número %d: ' % i)) for i in range(1,6)]
 
-  for i in vetor:
-    print i
+   for i in vetor:
+     print i
+  except ValueError:
+    print "O valor digitado dever ser inteiro"
 
 if __name__ == '__main__':
   lerImprimeVetor()
