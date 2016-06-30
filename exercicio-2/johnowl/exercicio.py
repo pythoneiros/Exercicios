ff1774c9b33957f022189d5847d23b3da616ecd2
João Paulo Gomes <Joo Paulo Gomes>
2016-06-29 23:51:13 -0300

    Exercício 2 / johnowl

---
+++ exercicio-2/johnowl/exercicio.py
@@ -0,0 +1,31 @@
+
+
+def printNumbers(numbers):
+    for n in numbers:
+        print(n, end=" ")
+
+
+entradas = []
+
+for i in range(10):
+    n = input("Digite um número: ")
+    entradas.append(n)
+
+pares = []
+impares = []
+
+for e in entradas:
+    if int(e) % 2 == 0:
+        pares.append(e)
+    else:
+        impares.append(e)
+
+
+print("Números pares: ", end="");
+printNumbers(pares)
+print("")
+print("Números ímpares: ", end="");
+printNumbers(impares)
+
+
+
