b75e1dc88ba1c65ac66093ad4c1f1a44f99f5488
Pedro-Souza <programadorhacker8@gmail.com>
2016-07-14 00:11:20 -0300

    Resolução - 5

---
+++ exercicio-5/Pedro-Souza/exercicio.py
@@ -0,0 +1,17 @@
+
+print("\n-----------------------------------------------------")
+print("Separador de pares e ímpares\n")
+
+numeros = input("Insira alguns números separados por espaço para \n que eu possa separar os pares dos ímpares\n\n:-) -> ").split( )
+
+pares, impares = [], [] 
+
+for numero in numeros:
+	if int(numero)%2 == 0:
+		pares.append(numero)
+	else:
+		impares.append(numero)
+
+print("\nNumeros pares: %s"  %(pares))
+print("Numeros impares: %s"  %(impares))
+
